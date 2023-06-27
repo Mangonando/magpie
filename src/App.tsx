94e0d4bf7dc090f8dc8dd5e3e94f10f824b83585
Mangonando <juan.f.ramirezv@gmail.com>
2023-06-27 22:54:18 +0200

    Fetch api create navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import NavBar from './components/NavBar/NavBar';
 
 const App: React.FC = () => {
-  const [setData, data] = React.useState<any[]>([])
+  const [data, setData] = useState<any>([]);
 
   useEffect(() => {
-    const fetchFakeStoreData = async () => {}
-  })
+    const fetchFakeStoreData = async () => {
+      try {
+        fetch('https://fakestoreapi.com/products/')
+          .then((res) => res.json())
+          .then((json) => {
+            setData(json);
+            console.log(json);
+          });
+      } catch (error) {
+        console.log('Error fetching API data', error);
+      }
+    };
+    fetchFakeStoreData();
+  }, []);
 
   return (
     <div>
-      <div>Magpie</div>
-      <button className="btn btn-primary">Check it!!</button>
+      <NavBar />
+      <div>
+        {data.map((product: { id: number; title: string; image: string }) => (
+          <div key={product.id}>
+            <div>{product.title}</div>
+            <img src={product.image} alt={product.title} />
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+export default function NavBar() {
+  return (
+    <>
+      <div className="nav-bar-container">
+        <div className="nav-bar-icon">Logo</div>
+        <div className="nav-bar-header">Magpie</div>
+      </div>
+    </>
+  );
+}
